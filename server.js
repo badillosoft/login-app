dc43347aa48cc56e38de1931a45f48e3188be7cf
badillosoft <badillo.soft@hotmail.com>
2018-02-06 11:37:01 -0600

    v1.0.3

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});  
+
 // Creamos una lista de usuarios (predefinida)
 const users = [{
     email: "batman@liga.com",
