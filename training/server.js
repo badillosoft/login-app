629b77bbbcc6937c4c568dd6fe20a01b68fffd90
badillosoft <badillo.soft@hotmail.com>
2018-02-06 09:44:11 -0600

    Training

--- training/server.js
+++ training/server.js
@@ -1,3 +1,17 @@
 const http = require("http");
 const express = require("express"); // npm install express
 
+// Creamos una instancia de servidor
+const app = express();
+
+// Definimos una ruta (el recurso /)
+app.get("/", (req, res) => {
+    // req - Request mantiene la información de la petición
+    // res - Response configura la respuesta al cliente
+    res.send("Hola mundo");
+});
+
+// Montar el servidor con http en el puerto 3000
+http.createServer(app).listen(3000, () => {
+    console.log("Servidor iniciado en http://localhost:3000/");
+});
